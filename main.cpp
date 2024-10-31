7bd055c48b4f9c3a83925ef021219011f1189f38
TommasoAngiuli <tommaso.angiuli@marconibari.edu.it>
2024-10-31 08:57:31 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -1,11 +1,23 @@
 #include <iostream>
 using namespace std;
-
-int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+int main() 
+{
+    int i;
+    double livello, somma, media;
+    somma = 0;
+    for (i = 1; i <= 7; i++) {
+        cout << "inserisci il livello dell'acqua" << endl;
+        cin >> livello;
+        somma = somma + livello;
+        if (livello > 10) {
+            cout << "nel giorno " << i << " l'acqua è alta" << endl;
+        }
+    }
+    media = somma / 7;
+    if (media > 0) {
+        cout << "l'acqua è alta" << endl;
+    } else {
+        cout << "il livello è normale" << endl;
+    }
+    cout << "il valore medio è " << media << endl;
 }
-
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
